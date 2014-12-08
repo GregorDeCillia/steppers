047b2b765b0604a7c03205b0cdb308992a059928
Gregor de Cillia <de.cillia.gregor@gmail.com>
2014-12-08 03:28:22 +0100

    the buffer logic seems to work, still got to test the buffer-reset triggers

--- abmStepper.cpp
+++ abmStepper.cpp
@@ -2,6 +2,8 @@
 #include <boost/numeric/ublas/vector.hpp>
 #include <boost/format.hpp>
 
+#include <boost/numeric/ublas/io.hpp>
+
 using namespace boost::numeric::ublas;
 
 typedef double value_type;
@@ -16,29 +18,49 @@ private:
 	int ord_;
 	unsigned int nStates_;
 	buffer_type buffer_;
+	int buffer_index_;
+	time_type h_;
 	time_type t_;
 	state_type x_;
 	state_type dx_;
+
+	void rk4step( time_type h ){
+		deriv_type k1 = dx_;
+		deriv_type k2 = f ( t_ + h/2.0 , x_ + k1*h/2.0 );
+		deriv_type k3 = f ( t_ + h/2.0 , x_ + k2*h/2.0 );
+		deriv_type k4 = f ( t_ + h     , x_ + k3*h ); 
+		x_ = x_ + h/6.0*( k1 + 2.0*k2 + 2.0*k3 + k4 );
+		t_ = t_ + h;
+		dx_ = f( t_, x_ );
+	}
 	
 
 public:
 	abmStepper( unsigned int nStates ) :
-		ord_(1),
-		nStates_(nStates){};
+		ord_( 1 ),
+		nStates_( nStates ),
+		buffer_( 4 ),
+		buffer_index_( 0 ){
+	};
 
 	state_type f(time_type t, state_type x){
 		value_type k = 100;
-		//x[0] = x[0];
 		return( x ); 
 	}
 
 	void setStates( time_type t, state_type x ){
-		if ( t == t_ && x[0] == x_[0] )
-			return;
+		if ( t == t_ )
+			{
+			bool flag = true;
+			for ( int i = 0; i < nStates_; i++ )
+				if ( x[i] != x_[i] ); flag = false;
+			if ( flag )
+				return;
+			}
 		t_ = t;
 		x_ = x;
 		dx_ = f( t_, x_);
-		buffer_.clear();
+		buffer_index_ = 0;
 	}
 	void getStates( time_type &t, state_type &x ){
 		t = t_;
@@ -61,34 +83,71 @@ public:
 		std::cout << std::endl;
 	}
 
-	void rk4step( time_type h ){
-		deriv_type k1 = dx_;
-		deriv_type k2 = f ( t_ + h/2.0 , x_ + k1*h/2.0 );
-		deriv_type k3 = f ( t_ + h/2.0 , x_ + k2*h/2.0 );
-		deriv_type k4 = f ( t_ + h     , x_ + k3*h ); 
-		x_ = x_ + h/6.0*( k1 + 2.0*k2 + 2.0*k3 + k4 );
-		t_ = t_ + h;
+	void printBuffer(){
+		std::cout << boost::format( "%-15E" ) % t_;
+		for ( unsigned int i = 0; i < nStates_; i++ ){
+			std::cout << boost::format( "%-15E" ) % buffer_[i][1];
+		}
+		std::cout << std::endl;		
 	}
 
-	void doStep(){
+
+	void doStep( time_type h ){
+		if ( h != h_ )
+			buffer_index_ = 0;
+		h_ = h;
+		//std::cout << buffer_ << std::endl;
+		//std::cout << buffer_.size() << std::endl;
+		if ( buffer_index_ < 4 )
+			{
+			rk4step( h );
+			buffer_.insert_element( buffer_index_,  dx_ );
+			buffer_index_++;
+			}
+		else{
+			// calculate the predictor
+			state_type xAB = x_ + h*(  55.0/24.0*buffer_[3] 
+			                           - 59.0/24.0*buffer_[2] 
+			                           + 37.0/24.0*buffer_[1] 
+			                           - 03.0/08.0*buffer_[0] );
+			t_+=h;
+			// make corrector step
+			for( int i = 0; i < ord_; i++ ){
+				xAB = x_ + h*(  251.0/720.0*f(t_,xAB)  + 646.0/720.0*buffer_[3]
+				               -264.0/720.0*buffer_[2] + 106.0/720.0*buffer_[1]
+				               -019.0/720.0*buffer_[0] );
+			}
+			x_ = xAB;
+			// cycle the buffer and fill in the new value
+			buffer_[0] = buffer_[1]; 
+			buffer_[1] = buffer_[2]; 
+			buffer_[2] = buffer_[3];
+			buffer_[3] = f( t_, x_ );
+			dx_ = buffer_[3];
+
+		}
 	}
 };
 
 int main(){
+	int nSteps = pow(2,5);
+	std::cout << "number of steps: " << nSteps << std::endl;
 	int nStates = 4;
 	abmStepper stepper( nStates );
 	time_type t,t2 = 0;
-	state_type x(nStates),x2(nStates);
+	state_type x(nStates), x2(nStates);
 	for ( int i = 0; i < nStates; i++ )
 		x[i]= i;
 	stepper.printLabels();
 	stepper.setStates(t,x);
 	stepper.printStates();
-	stepper.rk4step( 0.1 );
+	for ( int i = 0; i < nSteps; i++ ){
+		stepper.doStep( 1.0/nSteps );
+	}
 	stepper.printStates();
-	stepper.rk4step( 0.1 );
+	stepper.getStates( t, x2 );
+	t = t - 1; x2 = x2 - x*exp(1);
+	stepper.setStates(t, x2);
+	std::cout << "Errors:" << std::endl;
 	stepper.printStates();
-	stepper.rk4step( 0.1 );
-	stepper.printStates();
-
 }
