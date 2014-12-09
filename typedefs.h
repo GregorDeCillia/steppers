b779e435270bf52349af1eb0f3e99fb8931a6508
Gregor de Cillia <de.cillia.gregor@gmail.com>
2014-12-09 09:12:42 +0100

    just found out the typedefs.h file was not in the repo

---
+++ typedefs.h
@@ -0,0 +1,9 @@
+#include <boost/numeric/ublas/vector.hpp>
+
+using namespace boost::numeric::ublas;
+
+typedef double value_type;
+typedef value_type time_type;
+typedef vector<value_type> state_type;
+typedef state_type deriv_type;
+typedef vector<deriv_type> buffer_type;
