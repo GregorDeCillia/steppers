a935571249c9fca05490385a47f0e0f84dfaae23
Gregor de Cillia <de.cillia.gregor@gmail.com>
2014-12-12 02:05:51 +0100

    two new runge kutta methods: ode23Stepper with order 2 and ode34Stepper with order 3

---
+++ stepper/rkStepper/ode23Stepper.cpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "../../include/assigner.hpp"
+#include "rkStepperBase.h"
+
+class ode23Stepper: public rkStepper
+{
+
+public:
+	ode23Stepper( unsigned int nStates , rhs_type f ) :
+		rkStepper( nStates, f, "ode23", 1, 2 )
+	{
+          A << 0.5;
+
+          b << 0.0, 1.0;
+
+          //c << 0.5, 0.5, 1.0;
+          calculateC();
+	};
+};
---
+++ stepper/rkStepper/ode34Stepper.cpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "../../include/assigner.hpp"
+#include "rkStepperBase.h"
+
+class ode34Stepper: public rkStepper
+{
+
+public:
+	ode34Stepper( unsigned int nStates , rhs_type f ) :
+		rkStepper( nStates, f, "ode34", 2, 3 )
+	{
+		A << 0.50, 0.00, 
+			-1.00, 2.00;
+
+		b << 1.0/6.0, 4.0/6.0, 1.0/6.0;
+
+		//c << 0.5, 0.5;
+		calculateC();
+	};
+};
